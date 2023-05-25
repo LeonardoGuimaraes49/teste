421d16a8dc188ea710605ccd4ea9b884b9a68470
LeonardoGuimaraes49 <leonardo.guimarae.pb@compasso.com.br>
2023-05-25 01:20:32 -0300

    Atividade IALG vetor

---
+++ Desktop/ufla/IALG/2023.1/atividadeVetor.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanhoVetor = 5;
+    int vetor[tamanhoVetor];
+
+    for (int i = 0; i < tamanhoVetor; i++) {
+        int numero;
+        cin >> numero;
+
+        if (numero >= 0) {
+            vetor[i] = numero;
+        }
+        else{
+            i--;
+        }
+
+    }
+    int menor= 0;;
+    int pos=0;
+    for (int i = 0; i < tamanhoVetor; i++) {
+        if(vetor[i]< menor){
+        vetor[i]=menor;
+        cout<<vetor[i];
+        }
+    }
+    cout << endl;
+
+    for (int i = 0; i < tamanhoVetor; i++) {
+    }
+
+    for (int i = 0; i < tamanhoVetor; i++) {
+        for (int j = 0; j < tamanhoVetor; j++) {
+            cout << vetor[j] << " ";
+        }
+        cout << endl;
+            menor=-1;
+        if (vetor[i] != -1) {
+            vetor[i] = -1;
+        }
+    }
+
+    return 0;
+}
